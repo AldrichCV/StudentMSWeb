e044685a719c0d0850d083f4104d15eea822e93f
AldrichCV <aldrichvillablanca@dmc.edu.ph>
2025-05-08 12:30:51 +0800

    Web Version of Student Management System

---
+++ SIS/StudentInformationSystemWebApp/Controllers/StudentController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentInformationSystemWebApp.Models;
+
+namespace StudentInformationSystemWebApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        private static List<Student> studentsList = new List<Student>();
+        public IActionResult StudentList()
+        {
+            return View(studentsList);
+        }
+
+        [HttpGet]
+        public IActionResult CreateStudent()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        { 
+          
+           
+            int recordNumber = studentsList.Count + 1;
+            student.studentId = GenerateStudentId(recordNumber); 
+
+            studentsList.Add(student);
+
+            return RedirectToAction("StudentList");
+        }
+
+
+        public static string GenerateStudentId(int recordNumber)
+        {
+            string year = DateTime.Now.Year.ToString(); 
+            string paddedRecord = recordNumber.ToString("D4");
+
+            return $"{year}-{paddedRecord}";
+        }
+
+
+
+        [HttpGet]
+        public IActionResult UpdateStudent(string id)
+        {
+            var student = studentsList.FirstOrDefault(s => s.studentId == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student); 
+        }
+
+
+        [HttpPost]
+        public IActionResult UpdateStudent(Student updatedStudent)
+        {
+            var existing = studentsList.FirstOrDefault(s => s.studentId == updatedStudent.studentId);
+            if (existing != null)
+            {
+              
+                existing.firstName = updatedStudent.firstName;
+                existing.middleName = updatedStudent.middleName;
+                existing.lastName = updatedStudent.lastName;
+                existing.nameExtension = updatedStudent.nameExtension;
+                existing.gender = updatedStudent.gender;
+                existing.dateOfBirth = updatedStudent.dateOfBirth;
+                existing.department = updatedStudent.department;
+                existing.yearLevel = updatedStudent.yearLevel;
+            }
+
+            return RedirectToAction("StudentList");
+        }
+
+        [HttpGet]
+        public IActionResult DeleteStudent(string id)
+        {
+            var student = studentsList.FirstOrDefault(s => s.studentId == id);
+            if (student != null)
+            {
+                studentsList.Remove(student);
+            }
+
+            return RedirectToAction("StudentList");
+        }
+
+    }
+}
---
+++ SIS/StudentInformationSystemWebApp/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace StudentInformationSystemWebApp.Models
+{
+    public class Student
+    {
+        public string studentId { get; set; }
+        public string firstName { get; set; }
+        public string middleName { get; set; }
+        public string lastName { get; set; }
+        public string nameExtension { get; set; }
+        public string gender { get; set; }
+        public DateTime dateOfBirth { get; set; }
+        public string department { get; set; }
+        public string yearLevel { get; set; }
+    }
+}
